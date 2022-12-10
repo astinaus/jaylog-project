2b7038b1533a08f18af93078d3173d845d89d280
astinaus <slaayers0@gmail.com>
2022-12-10 18:07:06 +0900

    practice

---
+++ jaylog-fastapi/src/dto/post_dto.py
@@ -0,0 +1,57 @@
+from pydantic import BaseModel
+from datetime import datetime
+from entity.post_entity import PostEntity
+from entity.user_entity import UserEntity
+
+
+class _PostUser(BaseModel):
+    idx: int
+    id: str
+    profileImage: str
+
+    @staticmethod
+    def toDTO(userEntity: UserEntity):
+        return _PostUser(
+            idx=userEntity.idx,
+            id=userEntity.id,
+            profileImage=userEntity.profile_image
+        )
+
+
+class ResMainPost(BaseModel):
+    idx: int
+    thumbnail: str | None
+    title: str
+    summary: str
+    likeCount: int
+    createDate: datetime
+    user: _PostUser
+
+    @staticmethod
+    def toDTO(postEntity: PostEntity):
+        return ResMainPost(
+            idx=postEntity.idx,
+            thumbnail=postEntity.thumbnail,
+            title=postEntity.title,
+            summary=postEntity.summary,
+            likeCount=len(postEntity.likeEntitys),
+            createDate=postEntity.create_date,
+            user=_PostUser.toDTO(postEntity.userEntity)
+        )
+
+    class Config:
+        orm_mode = True
+
+
+class ReqInsertPost(BaseModel):
+    title: str
+    summary: str
+    content: str
+    thumbnail: str | None
+
+
+class ResInsertPost(BaseModel):
+    idx: int
+
+    class Config:
+        orm_mode = True
